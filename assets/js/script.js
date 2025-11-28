a3d3ae0d0fa496de102fdda8863d1f6187a2e343
Geraldine Morey <geraldinemorey@rocketmail.com>
2025-11-28 12:16:38 +0000

    Add navigation menu and toggle button, update styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
-console.log("test");
+// Wait for the DOM to finish loading before running functions
+document.addEventListener("DOMContentLoaded", function() {
+
+    //Get the navbar button and apply event listener
+    let navBtn = document.getElementById("menu-btn");
+
+    navBtn.addEventListener("click", function() {
+        document.getElementById("nav-menu").classList.toggle("hidden");
+    })
+})
