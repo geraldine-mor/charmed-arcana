173c4c97a895a4e11bb7ab0c858c06b3fbdb76bc
Geraldine Morey <geraldinemorey@rocketmail.com>
2025-12-03 16:42:45 +0000

    Get random card generation working

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,10 +19,23 @@ document.addEventListener("DOMContentLoaded", function() {
  */
 let clicks = 0; //defined outside the function so it doesn't reset every time
 function tarotGame() {
+    const cards = [
+        {src: "/assets/images/death.webp", alt: "Tarot card depicting death",},
+        {src: "/assets/images/judgement.webp", alt: "Tarot card depicting judgement"},
+        {src: "/assets/images/justice.webp", alt: "Tarot card depicting justice"},
+        {src: "/assets/images/strength.webp", alt: "Tarot card depicting strength"},
+        {src: "/assets/images/temperance.webp", alt: "Tarot card depicting temperance"},
+        {src: "/assets/images/the-magician.webp", alt: "Tarot card depicting the magician"},
+    ]
 
     let gameBtn = document.getElementById("game-btn");
+    let i = Math.floor(Math.random()*6);
 
         if (clicks === 0) {
+            let card = cards[i];
+            currentCard = card;
+            document.getElementById("game-card").setAttribute("src", card.src);
+            document.getElementById("game-card").setAttribute("alt", card.alt);
             document.getElementById("card-flip").classList.add("flipped");
             document.getElementById("game-text").classList.remove("hidden");
             gameBtn.innerText = "Draw next card";
