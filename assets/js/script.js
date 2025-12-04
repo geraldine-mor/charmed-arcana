461b33ca56590b9c16e55116b9541a184cfa9fb8
Geraldine Morey <geraldinemorey@rocketmail.com>
2025-12-04 12:38:57 +0000

    Add extra cards for game play on larger screens and adjust styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let gameBtn = document.getElementById("game-btn");
 
     gameBtn.addEventListener("click", tarotGame);
+
 })
 
 /**
@@ -20,12 +21,12 @@ document.addEventListener("DOMContentLoaded", function() {
 let clicks = 0; //defined outside the function so it doesn't reset every time
 function tarotGame() {
     const cards = [
-        {src: "/assets/images/death.webp", alt: "Tarot card depicting death",},
-        {src: "/assets/images/judgement.webp", alt: "Tarot card depicting judgement"},
-        {src: "/assets/images/justice.webp", alt: "Tarot card depicting justice"},
-        {src: "/assets/images/strength.webp", alt: "Tarot card depicting strength"},
-        {src: "/assets/images/temperance.webp", alt: "Tarot card depicting temperance"},
-        {src: "/assets/images/the-magician.webp", alt: "Tarot card depicting the magician"},
+        {src: "assets/images/death.webp", alt: "Tarot card depicting death",},
+        {src: "assets/images/judgement.webp", alt: "Tarot card depicting judgement"},
+        {src: "assets/images/justice.webp", alt: "Tarot card depicting justice"},
+        {src: "assets/images/strength.webp", alt: "Tarot card depicting strength"},
+        {src: "assets/images/temperance.webp", alt: "Tarot card depicting temperance"},
+        {src: "assets/images/the-magician.webp", alt: "Tarot card depicting the magician"},
     ]
 
     let gameBtn = document.getElementById("game-btn");
@@ -34,11 +35,11 @@ function tarotGame() {
         if (clicks === 0) {
             let card = cards[i];
             currentCard = card;
-            document.getElementById("game-card").setAttribute("src", card.src);
-            document.getElementById("game-card").setAttribute("alt", card.alt);
-            document.getElementById("card-flip").classList.add("flipped");
+            document.getElementById("left-game-card").setAttribute("src", card.src);
+            document.getElementById("left-game-card").setAttribute("alt", card.alt);
+            document.getElementById("left-card-flip").classList.add("flipped");
             document.getElementById("game-text").classList.remove("hidden");
-            gameBtn.innerText = "Draw next card";
+            gameBtn.innerText = "Reveal Second Card";
             clicks++;
         } else if (clicks === 1) {
             document.getElementById("card-flip").classList.remove("flipped");
